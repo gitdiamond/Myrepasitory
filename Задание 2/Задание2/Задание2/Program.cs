c65463854a90b6df87e415a5aba0934064b873e6
gitdiamond <kolov.efimka@gmail.com>
2023-02-17 01:05:18 +1000

    added file task

---
+++ Задание 2/Задание2/Задание2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+/*Console.Write("Введите массив строк, используя пробел:");
+string[] array;
+string array = Console.ReadLine();
+array = array.Split(' ');
+string result = new string[array.Length];
+string realSize = 0;
+
+foreach (string s in array)
+{
+    if(s.Length <=3)
+    {
+        result[realSize] = s;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(result, 0, realSize));
+Console.WriteLine(true);*/
+
+string[] text;
+Console.WriteLine("Введите массив строк через запятую");
+string array = Console.ReadLine();
+text = array.Split(' ');
+var result = new string[text .Length];
+var realSize = 0;
+foreach (var findi in text )
+{
+    if (findi.Length <= 3)
+    {
+        result[realSize] = findi;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
